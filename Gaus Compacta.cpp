b7365155dd07cba95bb1d4c0ffcb4c303abd2549
Paulo Henrique Dionysio <110944636+PauloPerigo@users.noreply.github.com>
2023-06-08 01:07:48 -0300

    Update Gaus Compacta.cpp

--- Gaus Compacta.cpp
+++ Gaus Compacta.cpp
@@ -1,114 +1,102 @@
 #include <stdio.h>
-#define max 10
 
-int somatorioU(int i, int j , int M[][max], int L[][max], int U[][max]){	//somatorio de U
-	int cont=0, k;
-	for(k=0 ; k < i ; k++){
-		cont+=L[i][k] * U[k][j]; 
-	}
-	
-	return cont;
+#define MAX 10
+
+double somatorioU(int i, int j, double M[][MAX], double L[][MAX], double U[][MAX]) {
+    double cont = 0.0;
+    int k;
+    for (k = 0; k < i; k++) {
+        cont += L[i][k] * U[k][j];
+    }
+    return cont;
 }
 
-void MU(int i, int j , int n, int M[][max], int L[][max], int U[][max]){	//Matriz de U
-	for(j=0 ; j < n ; j++){
-		if(i==0)U[i][j]=M[i][j];
-		else{
-		U[i][j]=M[i][j] - somatorioU(i, j, M, L ,U); 
-	}
-}
-}
-
-
-int somatorioL(int i, int j , int M[][max], int L[][max], int U[][max]){	//Somatorio de L
-	int cont=0, k;
-	for(k=0 ; k < j ; k++){
-		cont+=L[i][k] * U[k][j]; 
-	}
-	
-	return cont;
+void MU(int i, int j, int n, double M[][MAX], double L[][MAX], double U[][MAX]) {
+    for (j = 0; j < n; j++) {
+        if (i == 0) {
+            U[i][j] = M[i][j];
+			printf("\nMatriz U[%d][%d] = %.2lf\n", i, j, U[i][j]);
+        } else {
+            U[i][j] = M[i][j] - somatorioU(i, j, M, L, U);
+			printf("\nMatriz U[%d][%d] = %.2lf\n", i, j, U[i][j]);
+        }
+    }
 }
 
-void ML(int i, int j , int n, int M[][max], int L[][max], int U[][max]){	//Matriz de L
-	for(i=0 ; i < n ; i++){
-		if(j==0)L[i][j]=M[i][j]/U[j][j];
-		else{
-		L[i][j]=(M[i][j] - somatorioL(i, j, M, L ,U))/U[j][j]; 
-	}
-}
-}
-
-int somatorioB(int i, int j , int B[], int L[][max], int BU[]){		//somatorio de B
-	int cont=0, k;
-	for(k=0 ; k < i ; k++){
-		cont+=L[i][k] * BU[k]; 
-	}
-	
-	return cont;
+double somatorioL(int i, int j, double M[][MAX], double L[][MAX], double U[][MAX]) {
+    double cont = 0.0;
+    int k;
+    for (k = 0; k < j; k++) {
+        cont += L[i][k] * U[k][j];
+    }
+    return cont;
 }
 
-void BU(int i, int j , int n, int B[] , int L[][max], int BU[]){		//Matriz de B
-		if(i==0)BU[i]=B[i];
-		
-		else{
-		BU[i]= B[i]- somatorioB(i, j, B, L ,BU); 
-	}
-
+void ML(int i, int j, int n, double M[][MAX], double L[][MAX], double U[][MAX]) {
+    for (i = 0; i < n; i++) {
+        if (j == 0) {
+            L[i][j] = M[i][j] / U[j][j];
+			printf("\nMatriz L[%d][%d] = %.2lf\n", i, j, L[i][j]);
+        } else {
+            L[i][j] = (M[i][j] - somatorioL(i, j, M, L, U)) / U[j][j];
+			printf("\nMatriz L[%d][%d] = %.2lf\n", i, j, L[i][j]);
+        }
+    }
 }
 
-void MGC(int t, int M[][10], int X[], int B[]){		//matriz de gaus compacta
-	int i, j;	//operadores linha e coluna
-	int U[max][max], L[max][max], Bu[max];	//matriz U e L
-	
-	for(i=0, j=0 ; i<t ; i++){
-		MU(i, j, t, M, L, U);
-		j++;
-		ML(i, j, t, M, L, U);
-		BU(i, j, t, B, L, Bu);
-}
-		
-		//precisa resolver a equacao
+void BU(int i, int j, int n, double B[], double L[][MAX], double BU[]) {
+    BU[i] = B[i];
+    for (j = 0; j < i; j++) {
+        BU[i] -= L[i][j] * BU[j];
+    }
 }
 
-int main(){
-	
-	int A[max][max], X[max], B[max];
-	int i , n, j;
-	
-	printf("Digite a ordem da matriz: ");
-	scanf("%d", &n);
-	
-	printf("\nDigite a matriz\n");
-	for(i=0 ; i<n ; i++)
-	for(j=0 ; j<n ; j++)
-	scanf("%d", &A[i][j]);	
-	
-	printf("Digite o vetor X");
-	
-	for(i=0 ; i<n ; i++)
-	scanf("%d", &X[i]);
-	
-	printf("Digite o vetor B");
-	
-	for(i=0 ; i<n ; i++)
-	scanf("%d", &B[i]);
-	
-	printf("\nA matriz eh:");
-	for(i=0 ; i<n ; i++){
-	printf("\n");
-	for(j=0 ; j<n ; j++)
-	printf("%d ", A[i][j]);
-}
-	
-	printf("\nO vetor B eh: ");
-	
-	for(i=0 ; i<n ; i++)
-	printf("%d", B[i]);
-	
-	MGC(n, A, X, B);
+void MGC(int t, double M[][MAX], double X[], double B[]) {
+    int i, j;
+    double U[MAX][MAX], L[MAX][MAX], Bu[MAX];
 
-	printf("\nO vetor X eh: ");
-	
-	for(i=0 ; i<n ; i++)
-	printf("%d", X[i]);
+    for (i = 0, j = 0; i < t; i++) {
+        MU(i, j, t, M, L, U);
+        j++;
+        ML(i, j, t, M, L, U);
+        BU(i, j, t, B, L, Bu);
+    }
+
+    // Resolver o sistema linear
+    for (i = t - 1; i >= 0; i--) {
+        X[i] = Bu[i];
+        for (j = i + 1; j < t; j++) {
+            X[i] -= U[i][j] * X[j];
+        }
+        X[i] /= U[i][i];
+    }
+}
+
+int main() {
+    double A[MAX][MAX], X[MAX], B[MAX];
+    int i, n, j;
+
+    printf("Digite a ordem da matriz: ");
+    scanf("%d", &n);
+
+    printf("\nDigite a matriz\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%lf", &A[i][j]);
+        }
+    }
+
+    printf("\nDigite o vetor B:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%lf", &B[i]);
+    }
+
+    MGC(n, A, X, B);
+
+    printf("\nO vetor X eh:\n(");
+    for (i = 0; i < n; i++) {
+        printf(" %lf ", X[i]);
+    }
+	printf(")");
+    return 0;
 }
