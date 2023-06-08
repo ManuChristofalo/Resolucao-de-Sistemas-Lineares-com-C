74467f0959a67d17a95abd9d980c5c86c4111c9f
ManuChristofalo <102175989+ManuChristofalo@users.noreply.github.com>
2023-06-08 19:49:02 -0300

    Update Triangulo Inferior.cpp

--- Triangulo Inferior.cpp
+++ Triangulo Inferior.cpp
@@ -1,62 +1,62 @@
 #include <stdio.h>
 #define max 10
 
-int somatoria(int j, int i , int M[][10], int X[]){
-	int cont=0;
-	for( ; j < i-1 ; j++){
-		cont+=M[i+1][j] * X[j]; //é i+1 para fixar a linha, pois na chamada de função é i-1
+int somatoria (int j, int i , double M[][10], double X[]) {
+	int cont = 0;
+	
+	for (j=0; j < i ; j++) {
+		cont+=M[i][j] * X[j]; 
+		//é i+1 para fixar a linha, pois na chamada de função é i-1
 	}
 	
 	return cont;
 }
 
-void TriInferior(int t, int M[][10], int X[], int B[]){
-	int i, j, ii=1, jj;
-	for(i=0 ; i<t ; i++){
-			if(i==0) X[0]= B[0]/M  [0][0];
-			else X[i]= (B[i] - somatoria(0, i-1, M, X))/M[i][i];
-}
+void TriInferior (int t, double M[][10], double X[], double B[]) {
+	int i, j;
+	
+	for (i=0; i<t; i++) {
+			if(i==0) X[0]= B[0]/M [0][0];
+			
+			else X[i]= (B[i] - somatoria(0, i, M, X))/M[i][i];
+	}
 }
 
-int main(){
+int main() {
 	
-	int A[max][max], X[max], B[max];
-	int i , n, j;
+	double A[max][max], X[max], B[max];
+	int i, n, j;
 	
 	printf("Digite a ordem da matriz: ");
 	scanf("%d", &n);
 	
-	printf("\nDigite a matriz\n");
+	printf("\nDigite a matriz %d x %d:\n", n, n);
 	for(i=0 ; i<n ; i++)
-	for(j=0 ; j<n ; j++)
-	scanf("%d", &A[i][j]);	
+		for(j=0 ; j<n ; j++)
+			scanf("%lf", &A[i][j]);	
 	
-	printf("Digite o vetor X");
-	
-	for(i=0 ; i<n ; i++)
-	scanf("%d", &X[i]);
-	
-	printf("Digite o vetor B");
+	printf("Digite o vetor B\n");
 	
 	for(i=0 ; i<n ; i++)
-	scanf("%d", &B[i]);
+		scanf("%lf", &B[i]);
 	
-	printf("\nA matriz eh:");
+	printf("\n\nA matriz inserida foi:");
+	
 	for(i=0 ; i<n ; i++){
-	printf("\n");
-	for(j=0 ; j<n ; j++)
-	printf("%d ", A[i][j]);
-}
+		printf("\n");
+			for(j=0 ; j<n ; j++)
+				printf("%.4lf ", A[i][j]);
+	}
 	
-	printf("\nO vetor B eh: ");
+	printf("\n\nO vetor B inserido foi: ");
 	
 	for(i=0 ; i<n ; i++)
-	printf("%d", B[i]);
+		printf("%.4lf ", B[i]);
 	
 	TriInferior(n, A, X, B);
 
-	printf("\nO vetor X eh: ");
+	printf("\n\nO vetor X sera: ");
 	
 	for(i=0 ; i<n ; i++)
-	printf("%d", X[i]);
+		printf("%.4lf ", X[i]);
 }
